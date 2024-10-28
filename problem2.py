61a92d1ba5bd0bfb1a31341262ff544fb66966cd
Shounauk Ganguli <rishgan08@gmail.com>
2024-10-28 22:34:10 +0000

    wrote factorial code

--- problem2.py
+++ problem2.py
@@ -1,1 +1,14 @@
+def factorial(n):
 
+    if n < 0:
+        return "undefined"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+
+print(factorial(5))  # returns 120
