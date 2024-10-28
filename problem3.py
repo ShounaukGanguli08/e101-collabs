0863beccb3e809fd46792edd55eaca094c85c271
lucy!! <lucythebigrat@gmail.com>
2024-10-28 17:45:33 -0500

    Update problem3.py: 1
    I made a function that will check for the largest number in the list that you enter in the parentheses for it!!

--- problem3.py
+++ problem3.py
@@ -1,1 +1,8 @@
+list = [1, 2, 3, 4, 5, 6, 7, 2332442, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+def findMax(listToCheck):
+  largestNumberInList = max(listToCheck)
+
+  print(f'The largest number in the list that you want to check is: {largestNumberInList}')
+
+findMax(list)
